78a486ba1766f5c1807250f685a2de8fa795cad0
sunkidz-jpg <quanz.1978@gmail.com>
2026-04-03 01:10:21 +0700

    upload source code

---
+++ hd_ft/main.js
@@ -0,0 +1,11 @@
+function loadHTML(id, file) {
+    fetch(file)
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        });
+}
+
+// Load chung cho mọi trang
+loadHTML("header", "/hd_ft/header.html");
+loadHTML("footer", "/hd_ft/footer.html");
